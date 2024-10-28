d422e3a915e743541a0d7d498168b7b1d6a3d4ab
subash-khanal <subash.khanal.cs@gmail.com>
2024-10-28 15:22:40 -0500

    update config

--- geoclap/config.py
+++ geoclap/config.py
@@ -12,7 +12,3 @@ cfg.log_dir = "path_to/logs"
 ################################### WEBDATASET PATHS ###########################################################
 cfg.GeoSound_webdataset_path = "path_to/GeoSound_for_mapping"
 cfg.SoundingEarth_webdataset_path = "path_to/SoundingEarth_for_mapping_fairsplit"
-
-cfg.llava_caption_for_sentinel = "path_to/llava_caption_for_sentinel.json"
-cfg.llava_caption_for_bingmap = "path_to/llava_caption_for_bingmap.json"
-cfg.llava_caption_for_SoundingEarth = "path_to/SoundingEarth_llava_caption_for_googleEarth.json"
